22a7f3301fc7c2650e4a84310a855cddc8199189
Vaibhav <01vatri@gmail.com>
2020-01-28 22:51:51 +0530

    fixed typo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
-app.use(bodyParser.urlencoded({extented: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function() {
   console.log("Server running on port 3000");

1fcc50b544cc01fc27e39f441a0995b873a4ad64
Vaibhav <01vatri@gmail.com>
2020-01-29 11:35:58 +0530

    redirect to signup page if failed

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post("/", function(req, res) {
     headers: {
       "Authorization": "Vaibhav 419548220020dbc1a0a1003cee22abfe-us4"
     },
-    body: json
+    //body: json
   };
 
   request(options, function(error, response, body) {
@@ -52,6 +52,10 @@ app.post("/", function(req, res) {
         res.sendFile(__dirname + "/failure.html");
     }
   });
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
